901e5fc56aae964b4566c24491a6f0e5a3b998c8
sieunkim <sieunkr@gmail.com>
2019-01-09 23:37:35 +0900

    테스트 코드

--- 04-reactor-create/src/test/java/com/example/demo/DemoApplicationTests.java
+++ 04-reactor-create/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,8 +2,15 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.reactivestreams.Subscription;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.SynchronousSink;
+
+import java.util.Random;
+import java.util.function.Consumer;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +18,14 @@ public class DemoApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void 테스트01(){
+
+        Flux.generate(null, (s, o) -> s, s -> {
+        });
+
     }
 
 }
